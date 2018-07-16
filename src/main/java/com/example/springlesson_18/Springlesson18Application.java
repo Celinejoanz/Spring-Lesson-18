ab752acaa992441742a36580406ed80e6a520429
Chidimma Uba <chidimmajuba@gmail.com>
2018-07-16 11:29:19 -0400

    first commit

---
+++ src/main/java/com/example/springlesson_18/Springlesson18Application.java
@@ -0,0 +1,12 @@
+package com.example.springlesson_18;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Springlesson18Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Springlesson18Application.class, args);
+	}
+}
